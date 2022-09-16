9d76acb4c3eb3e447a2a179673568578eac480d9
moneymike22 <78703707+moneymike22@users.noreply.github.com>
2022-09-16 15:08:30 -0500

    56

--- src/database.js
+++ src/database.js
@@ -1,138 +1,138 @@
 module.exports = {
-  "1": {
+  "0": {
     "name": "Apu",
     'level': Math.floor(Math.random() * 99) + 1,
     'speed': Math.floor(Math.random() * 15) + 2,
     "health": Math.floor(Math.random() * 32) + 19,
     'strength': Math.floor(Math.random() * 50) + 1
   },
-  "2": {
+  "1": {
     "name": "Pepe",
     'level': Math.floor(Math.random() * 99) + 1,
     'speed': Math.floor(Math.random() * 20) + 2,
     "health": Math.floor(Math.random() * 56) + 32,
+    'strength': Math.floor(Math.random() * 50) + 1
+  },
+  "2": {
+    "name": "Bobo",
+    'level': Math.floor(Math.random() * 99) + 1,
+    'speed': Math.floor(Math.random() * 20) + 2,
+    "health": Math.floor(Math.random() * 51) + 22,
     'strength': Math.floor(Math.random() * 50) + 1
   },
   "3": {
-    "name": "Bobo",
+    "name": "Bort",
     'level': Math.floor(Math.random() * 99) + 1,
     'speed': Math.floor(Math.random() * 20) + 2,
-    "health": Math.floor(Math.random() * 51) + 22,
+    "health": Math.floor(Math.random() * 44) + 14,
     'strength': Math.floor(Math.random() * 50) + 1
   },
   "4": {
-    "name": "Bort",
+    "name": "Wojak",
     'level': Math.floor(Math.random() * 99) + 1,
     'speed': Math.floor(Math.random() * 20) + 2,
-    "health": Math.floor(Math.random() * 44) + 14,
+    "health": Math.floor(Math.random() * 20) + 4,
     'strength': Math.floor(Math.random() * 50) + 1
   },
   "5": {
-    "name": "Wojak",
+    "name": "Apu",
     'level': Math.floor(Math.random() * 99) + 1,
     'speed': Math.floor(Math.random() * 20) + 2,
-    "health": Math.floor(Math.random() * 20) + 4,
+    "health": Math.floor(Math.random() * 32) + 19,
     'strength': Math.floor(Math.random() * 50) + 1
   },
   "6": {
-    "name": "Apu",
+    "name": "Pepe",
     'level': Math.floor(Math.random() * 99) + 1,
     'speed': Math.floor(Math.random() * 20) + 2,
-    "health": Math.floor(Math.random() * 32) + 19,
+    "health": Math.floor(Math.random() * 56) + 32,
     'strength': Math.floor(Math.random() * 50) + 1
   },
   "7": {
-    "name": "Pepe",
+    "name": "Bobo",
     'level': Math.floor(Math.random() * 99) + 1,
     'speed': Math.floor(Math.random() * 20) + 2,
-    "health": Math.floor(Math.random() * 56) + 32,
+    "health": Math.floor(Math.random() * 51) + 22,
     'strength': Math.floor(Math.random() * 50) + 1
   },
   "8": {
-    "name": "Bobo",
+    "name": "Bort",
     'level': Math.floor(Math.random() * 99) + 1,
     'speed': Math.floor(Math.random() * 20) + 2,
-    "health": Math.floor(Math.random() * 51) + 22,
+    "health": Math.floor(Math.random() * 44) + 14,
     'strength': Math.floor(Math.random() * 50) + 1
   },
   "9": {
-    "name": "Bort",
+    "name": "Wojak",
     'level': Math.floor(Math.random() * 99) + 1,
     'speed': Math.floor(Math.random() * 20) + 2,
-    "health": Math.floor(Math.random() * 44) + 14,
+    "health": Math.floor(Math.random() * 20) + 4,
     'strength': Math.floor(Math.random() * 50) + 1
   },
   "10": {
-    "name": "Wojak",
+    "name": "Apu",
     'level': Math.floor(Math.random() * 99) + 1,
     'speed': Math.floor(Math.random() * 20) + 2,
-    "health": Math.floor(Math.random() * 20) + 4,
+    "health": Math.floor(Math.random() * 32) + 19,
     'strength': Math.floor(Math.random() * 50) + 1
   },
   "11": {
-    "name": "Apu",
-    'level': Math.floor(Math.random() * 99) + 1,
-    'speed': Math.floor(Math.random() * 20) + 2,
-    "health": Math.floor(Math.random() * 32) + 19,
-    'strength': Math.floor(Math.random() * 50) + 1
-  },
-  "12": {
     "name": "Pepe",
     'level': Math.floor(Math.random() * 99) + 1,
     'speed': Math.floor(Math.random() * 20) + 2,
     "health": Math.floor(Math.random() * 56) + 32,
     'strength': Math.floor(Math.random() * 50) + 1
   },
-  "13": {
+  "12": {
     "name": "Bobo",
     'level': Math.floor(Math.random() * 99) + 1,
     'speed': Math.floor(Math.random() * 20) + 2,
     "health": Math.floor(Math.random() * 51) + 22,
     'strength': Math.floor(Math.random() * 50) + 1
   },
-  "14": {
+  "13": {
     "name": "Bort",
     'level': Math.floor(Math.random() * 99) + 1,
     'speed': Math.floor(Math.random() * 20) + 2,
     "health": Math.floor(Math.random() * 44) + 14,
     'strength': Math.floor(Math.random() * 50) + 1
   },
-  "15": {
+  "14": {
     "name": "Wojak",
     'level': Math.floor(Math.random() * 99) + 1,
     'speed': Math.floor(Math.random() * 20) + 2,
     "health": Math.floor(Math.random() * 20) + 4,
     'strength': Math.floor(Math.random() * 50) + 1
   },
-  "16": {
+  "15": {
     "name": "Apu",
     'level': Math.floor(Math.random() * 99) + 1,
     'speed': Math.floor(Math.random() * 20) + 2,
     "health": Math.floor(Math.random() * 32) + 19,
     'strength': Math.floor(Math.random() * 50) + 1
   },
-  "17": {
+  "16": {
     "name": "Pepe",
     'level': Math.floor(Math.random() * 99) + 1,
     'speed': Math.floor(Math.random() * 20) + 2,
     "health": Math.floor(Math.random() * 56) + 32,
     'strength': Math.floor(Math.random() * 50) + 1
   },
-  "18": {
+  "17": {
     "name": "Bobo",
     'level': Math.floor(Math.random() * 99) + 1,
     'speed': Math.floor(Math.random() * 20) + 2,
     "health": Math.floor(Math.random() * 51) + 22,
     'strength': Math.floor(Math.random() * 50) + 1
   },
-  "19": {
+  "18": {
     "name": "Bort",
     'level': Math.floor(Math.random() * 99) + 1,
     'speed': Math.floor(Math.random() * 20) + 2,
     "health": Math.floor(Math.random() * 44) + 14,
     'strength': Math.floor(Math.random() * 50) + 1
   },
-  "20": {
+  "19": {
     "name": "Wojak",
     'level': Math.floor(Math.random() * 99) + 1,
     'speed': Math.floor(Math.random() * 20) + 2,
